2bbb4345a73fbeea8c92a90e222dc924e224ccde
Boryagin Konstantin <konstantin.boryagin@innopolis.ru>
2022-07-24 15:32:06 +0300

    security with DB and login page

--- src/main/java/com/example/tacocloud/security/SecurityConfig.java
+++ src/main/java/com/example/tacocloud/security/SecurityConfig.java
@@ -1,14 +1,19 @@
 package com.example.tacocloud.security;
 
+import com.example.tacocloud.data.UserRepository;
+import com.example.tacocloud.tacos.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -17,18 +22,41 @@ import java.util.List;
 @Configuration
 public class SecurityConfig {
 
+//    private UserRepository userRepository;
+//
+//    @Autowired
+//    public void setUserRepository(UserRepository userRepository) {
+//        this.userRepository = userRepository;
+//    }
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
     @Bean
-    public UserDetailsService userDetailsService(PasswordEncoder passwordEncoder) {
-        List<UserDetails> userDetails = new ArrayList<>();
-        userDetails.add(new User("buzz", passwordEncoder().encode("password"),
-                Arrays.asList(new SimpleGrantedAuthority("ROLE_USER"))));
-        userDetails.add(new User("woddy", passwordEncoder().encode("password"),
-                Arrays.asList(new SimpleGrantedAuthority("ROLE_USER"))));
-        return new InMemoryUserDetailsManager(userDetails);
+    public UserDetailsService userDetailsService(UserRepository userRepo) {
+       return username -> {
+           User user = userRepo.findByUsername(username);
+           if(user != null) {
+               return user;
+           } else {
+               throw new UsernameNotFoundException("User '" + username + "' not found");
+           }
+       };
     }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http.authorizeRequests()
+                .antMatchers("/design", "/order").access("hasRole('USER')")
+                .antMatchers("/", "/**").access("permitAll()")
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .defaultSuccessUrl("/design")
+                .and()
+                .build();
+    }
+
 }
